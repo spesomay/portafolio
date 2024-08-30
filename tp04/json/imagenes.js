25e37385e18bdebef213e3846594810c1a9ce4a6
spesomay <163929987+spesomay@users.noreply.github.com>
2024-08-29 21:05:50 -0300

    Update imagenes.js

--- tp04/json/imagenes.js
+++ tp04/json/imagenes.js
@@ -6,19 +6,38 @@ function pintarDatos(coso){
     .then(response => {
         return response.json();
     })
-    .then(cate => {
-        
+    .then(cate => {        
         cate.forEach(element => {
             // alert(nombreResto)
             if(element.nombre === nombreResto){
-                localStorage.clear();
-                localStorage.setItem("img", element.img);
+                localStorage.clear(); // Limpiar localStorage antes de guardar nuevos datos
+
+                // Guardar datos en localStorage
+                localStorage.setItem("nombre", element.nombre);
                 localStorage.setItem("origen", element.origen);
-                console.log(localStorage.getItem("img"))
+                localStorage.setItem("descripcion", element.descripcion);
+                localStorage.setItem("direccion", element.direccion);
+                localStorage.setItem("estrellas", element.estrellas);
+                localStorage.setItem("img", element.img);
+
+                // Guardar imágenes adicionales del restaurante
+                localStorage.setItem("img1", element.resto.img1);
+                localStorage.setItem("img2", element.resto.img2);
+                localStorage.setItem("img3", element.resto.img3);
+                localStorage.setItem("img3", element.resto.img4);
+
+                // Guardar menú
+                localStorage.setItem("menu1", element.menu.nombre);
+                localStorage.setItem("precio1", element.menu.precio);
+                localStorage.setItem("menu2", element.menu.nombre2);
+                localStorage.setItem("precio2", element.menu.precio2);
+                localStorage.setItem("menu3", element.menu.nombre);
+                localStorage.setItem("precio3", element.menu.precio);
+
+                console.log("Datos guardados en localStorage");
             }
         });
     });
-    // .catch(error => {
-    //     alert('Error al obtener los datos:', error);
-    // });
+
+    
 }
